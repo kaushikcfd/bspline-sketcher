a2c3a406cdb64b038700d93fc06cf19bab2b66d6
Kaushik Kulkarni <kaushikcfd@gmail.com>
2020-04-06 18:15:59 -0500

    adds lines between previous 2 points

--- slate.js
+++ slate.js
@@ -2,6 +2,10 @@
 // https://web.mit.edu/hyperbook/Patrikalakis-Maekawa-Cho/node17.html
 // Next TODO:
 // You keep on clicking and we start getting lines that connect the points.
+// [X] Click on a point and generate a 'dot' for it.
+// [X] Draw a line between the penultimate and ultimate point.
+// [ ] As long as we are in draw mode... have a line floating from one point to the next, wh
+// We've something which generates a point for every click. Now just ma
 
 const SlateModes = {
   VISUAL: 'visual',
@@ -45,15 +49,16 @@ class DrawingContext {
 const slateDiv = document.getElementById("slatepaper");
 const slateWidth = slateDiv.clientWidth;
 const slateHeight = slateDiv.clientHeight;
+const pointRadius = 6;
 
 // Create the paper
 const paper = Raphael("slatepaper", slateWidth, slateHeight);
 
 // Read keyboard events
-document.addEventListener('keydown', logKey);
+document.addEventListener('keydown', keydownHandler);
 
 // Top level function which triggers which event to execute based on user behavior.
-function logKey(e) {
+function keydownHandler(e) {
   if (e.key == 'd') {
     if (slateMode == SlateModes.VISUAL) {
       slateMode = SlateModes.DRAW;
@@ -82,12 +87,30 @@ function logKey(e) {
 
 
 function drawClickHandler(e) {
-  const clickX = e.offsetX;
-  const clickY = e.offsetY;
+  // FIXME: offset is not a good choice, as once we have a different
+  // 'div' the offset would change and abandon it.  A better metric is
+  // always clientX, clientY.
+  const clickX = e.clientX - slateDiv.getBoundingClientRect().left;
+  const clickY = e.clientY - slateDiv.getBoundingClientRect().top;
 
-  //@TODO: '2' is no good, set the radius in a relative sense.
-  var circle = paper.circle(clickX, clickY, 2);
+  console.log(`( ${ clickX },  ${clickY}`);
+
+  //@TODO: '10' is no good, set the radius in a relative sense.
+  var circle = paper.circle(clickX, clickY, pointRadius);
   circle.attr("fill", "#00f");
+  if (lastPoint != null) {
+    lastPointX = lastPoint.getPointAtLength().x;
+    lastPointY = lastPoint.getPointAtLength().y + pointRadius;
+
+    // FIXME: There appears to be some offset in the path creation.
+    var pathString = `M ${ lastPointX } ${ lastPointY } L ${ clickX } ${ clickY }`;
+    paper.path(pathString);
+  }
+  else {
+    console.log("Fail bhai!");
+  }
+
+  lastPoint = circle;
 }
 
 
